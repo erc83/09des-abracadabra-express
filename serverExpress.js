2e98b8ced1f7a7ebcb5c105270e5eb72a5a9c6c3
erc83 <espinoza.eric@gmail.com>
2021-02-04 14:06:30 -0300

    termino de aplicacion

--- serverExpress.js
+++ serverExpress.js
@@ -3,7 +3,6 @@
 const express = require('express')
 const app = express()
 
-const usuarios = ["eric", "fabiola", "elias", "elena"]
 //servidor disponible
 app.listen(3000, ()=> {
     console.log('localhost/servidorExpress OK')
@@ -11,19 +10,19 @@ app.listen(3000, ()=> {
 
 app.use(express.static('assets'))
 
+const usuarios = ["eric", "fabiola", "elias", "elena"]
+
+
 app.get("/abracadabra/usuarios", (req, res)=> {
-    console.log(JSON.stringify(usuarios))
-    res.send(JSON.stringify(usuarios))
+    res.send(usuarios)
 })
 
 
-app.use("/abracadabra/juego/:usuario ", (req, res, next) => {
-    if (usuarios.includes(req.params.usuarios)){
-        console.log(req.params.usuario)
-        next()
-    }else{
-        res.sendFile(__dirname + "/assets/who.jpeg")
-    }
+app.use("/abracadabra/juego/:usuario", (req, res, next) => {
+    const { usuario } = req.params;
+    console.log(req.params)  
+    usuario == "eric" || usuario == "fabiola" || usuario == "elias" || usuario == "elena" ?
+    next() : res.sendFile(__dirname + "/assets/who.jpeg")
 })
 
 app.get("/abracadabra/juego/:usuario", (req, res)=>{
@@ -31,21 +30,28 @@ app.get("/abracadabra/juego/:usuario", (req, res)=>{
 })
 
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html")
-})
 
-app.use("abracadabra/conejo/:n", (req, res, next) => {
+app.get("/abracadabra/conejo/:n", (req, res, next) => {
     const numAleatorio = Math.floor(Math.random() * 4) + 1
     const numParam = Number(req.params.n)
+    console.log(numParam)
     if ( numAleatorio === numParam){
-        res.sendFile(__dirname + "/assets/conejito.jpg")
+        res.sendFile(__dirname + "/assets/conejito.jpeg")
     }else{
-        res.sendFile(__dirname + "/assets/voldemort.jpg")
+        res.sendFile(__dirname + "/assets/voldemort.jpeg")
     }
 })
 
 // para todo acceso que no tenga una ruta
-app.get("*", (req, res)=>{
-    res.send("<center><h1>No existe la ruta</h1></center>")
+app.get("*", (req, res) => {
+    res.send(`<center>
+    <h1>La ruta no Existe</h1>
+    <h2>Para ingresar seleccione uno de los siguientes links</h2>
+    <h2><a href="http://localhost:3000/abracadabra/juego/eric">Ingreso Abracadabra como usuario eric</a></h2>
+    <h2><a href="http://localhost:3000/abracadabra/juego/elena">Ingreso Abracadabra como usuario elena</a></h2>
+    <h2><a href="http://localhost:3000/abracadabra/juego/fabiola">Ingreso Abracadabra como usuario fabiola</a></h2>
+    <h2><a href="http://localhost:3000/abracadabra/juego/elias">Ingreso Abracadabra como usuario elias</a></h2>
+    <h2><a href="http://localhost:3000/abracadabra/usuarios">revisar los usuarios</a></h2>
+    </center>    
+    `);
 })
